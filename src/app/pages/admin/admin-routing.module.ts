d24a868d38ac59e1e6c723b54ea84d3db578eb0d
Murillo Oliveira <murillo1047@gmail.com>
2024-10-21 16:02:22 -0300

    refactor: update admin routing module with new routes

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminHomepageComponent } from '../admin-homepage/admin-homepage.component';
 import { AdminComponent } from './admin.component';
 import { AdminCadastrarPacienteComponent } from '../admin-cadastrar-paciente/admin-cadastrar-paciente.component';
+import { AdminCadastrarServicosComponent } from '../admin-cadastrar-servicos/admin-cadastrar-servicos.component';
+import { AdminAgendarExameComponent } from '../admin-agendar-exame/admin-agendar-exame.component';
+import { AdminCadastrarHospitalComponent } from '../admin-cadastrar-hospital/admin-cadastrar-hospital.component';
+import { AdminCadastrarMedicoComponent } from '../admin-cadastrar-medico/admin-cadastrar-medico.component';
+import { AdminCadastrarAdminComponent } from '../admin-cadastrar-admin/admin-cadastrar-admin.component';
 
 const routes: Routes = [
   {
@@ -15,6 +20,21 @@ const routes: Routes = [
     },
     {
       path: 'cadastrar-paciente', component: AdminCadastrarPacienteComponent
+    },
+    {
+      path: 'cadastrar-servico', component: AdminCadastrarServicosComponent
+    },
+    {
+      path: 'cadastrar-hospital', component: AdminCadastrarHospitalComponent
+    },
+    {
+      path: 'cadastrar-medico', component: AdminCadastrarMedicoComponent
+    },
+    {
+      path: 'cadastrar-colaborador', component: AdminCadastrarAdminComponent
+    },
+    {
+      path: 'agendar-exame/:id', component: AdminAgendarExameComponent
     },
    ]
   },
