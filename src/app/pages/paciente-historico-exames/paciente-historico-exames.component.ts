ac9cb96070e82f56136158f8fd2c1ad4ba77cb9c
Murillo Oliveira <murillo1047@gmail.com>
2024-10-27 15:23:47 -0300

    feat: improve data display and added buttonsa to diferentiated exams to appointments

--- src/app/pages/paciente-historico-exames/paciente-historico-exames.component.ts
+++ src/app/pages/paciente-historico-exames/paciente-historico-exames.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Exame } from 'src/app/models/exame';
+import { Paciente } from 'src/app/models/paciente';
 
 @Component({
   selector: 'app-paciente-historico-exames',
@@ -7,6 +9,66 @@ import { Component } from '@angular/core';
 })
 export class PacienteHistoricoExamesComponent {
   selectedButton: string | null = "anteriores";
+
+  examesConcluidos: Exame[] = [
+    new Exame(
+      1,
+      'Exame de Sangue',
+      'Concluido',
+      'Laboratório A',
+      new Date('2024-10-09').toLocaleDateString('pt-BR'),
+      new Paciente(
+        0,
+        "Luana Camila",
+        "Feminino",
+        "5646578945132",
+        new Date('04/11/2005'),
+      ),
+      'caminho/para/o/s3',
+      'Dor no peito',
+      'Jejum de 8 horas',
+      '/assets/images/resultado1.jfif'
+    ),
+    new Exame(
+      3,
+      'Raio-X',
+      'Concluido',
+      'Laboratório C',
+      new Date('2024-10-07').toLocaleDateString('pt-BR'),
+      new Paciente(
+        0,
+        "Luana Camila",
+        "Feminino",
+        "5646578945132",
+        new Date('04/11/2005'),
+      ),
+      'caminho/para/o/s3',
+      'Muita dor no braço',
+      'Nenhum pré-preparo',
+      '/assets/images/resultado3.jfif'
+    ),
+  ];
+
+  examesAgendados: Exame[] = [
+    new Exame(
+      2,
+      'Exame de Urina',
+      'Agendado',
+      'Laboratório B',
+      new Date('2024-10-08').toLocaleDateString('pt-BR'),
+      new Paciente(
+        0,
+        "Luana Camila",
+        "Feminino",
+        "5646578945132",
+        new Date('04/11/2005'),
+      ),
+      'caminho/para/o/s3',
+      'Dor no membro',
+      'Jejum de 8 horas',
+      null // Exame agendado, sem URL de resultado
+    ),
+  ];
 
   selectButton(button: string) {
     this.selectedButton = button;
