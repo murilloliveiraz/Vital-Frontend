756ef2549e819965e6dd4e1eb3480a24498d6ac9
Murillo Oliveira <murillo1047@gmail.com>
2024-11-04 16:16:03 -0300

    feat: payment classes

---
+++ src/app/models/pagamentos/CreditCardPayment.ts
@@ -0,0 +1,13 @@
+export class CreditCardPayment {
+  transactionAmount: number;
+    token: string;
+    nomeServico: string;
+    valorConsulta: number;
+    installments: number;
+    consultaId: number;
+    nomePagador: string;
+    paymentMethodId: string;
+    emailPagador: string;
+    type: string;
+    number: string;
+}
---
+++ src/app/models/pagamentos/PixPayment.ts
@@ -0,0 +1,9 @@
+export class PixPayment {
+  emailPagador: string;
+    nomePagador: string;
+    cpfPagador: string;
+    sobrenomePagador: string;
+    nomeServico: string;
+    consultaId: number;
+    valorConsulta: number;
+}
