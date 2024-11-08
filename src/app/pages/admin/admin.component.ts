abd4f128534b268b38ac081fd55292bb88a07efe
Murillo Oliveira <murillo1047@gmail.com>
2024-11-08 20:24:56 -0300

    refactor: renaming register pattern name routes

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AdministradorService } from 'src/app/services/administrador.service';
+import { AuthService } from './../../services/auth.service';
+import { AdminResponseContract } from 'src/app/models/administrador/adminResponseContract';
 
 @Component({
   selector: 'app-admin',
@@ -6,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  constructor(private administradorService: AdministradorService, private authService: AuthService){}
   isOpen = false;
+  admin: AdminResponseContract;
+
+  ngOnInit(){
+    const email = this.authService.getEmailUser();
+
+    const observer = {
+      next: (admin: AdminResponseContract) => {
+        this.admin = admin;
+      },
+      error: (err: any) => {
+        alert('Ocorreu um erro');
+      }
+    }
+
+    this.administradorService.getByEmail(email).subscribe(observer);
+    };
 
   toggleSidenav() {
     this.isOpen = !this.isOpen;
