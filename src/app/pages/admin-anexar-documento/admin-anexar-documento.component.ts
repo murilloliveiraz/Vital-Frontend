b40fa40ee272ea9b6f1f45a00e8696510722fa2e
Murillo Oliveira <murillo1047@gmail.com>
2024-11-09 23:12:01 -0300

    feat: integration to attach document to appoinments and exams

--- src/app/pages/admin-anexar-documento/admin-anexar-documento.component.ts
+++ src/app/pages/admin-anexar-documento/admin-anexar-documento.component.ts
@@ -1,7 +1,11 @@
 import { Location } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { ExamesService } from './../../services/exames.service';
+import { ExameConcluidoResponse } from './../../models/exame/ExameConcluidoResponse';
+import { DocumentoService } from './../../services/documento.service';
+import { Documento } from 'src/app/models/documento';
 
 @Component({
   selector: 'app-admin-anexar-documento',
@@ -12,9 +16,12 @@ export class AdminAnexarDocumentoComponent {
   agendamento: string;
   tipoAgendamento: string;
   selectedFileName: string | null = null;
+  selectedFile: File;
   selectedType: string = null;
+  fileErrors: boolean = false;
+  urlexterna: string = "";
 
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(private route: ActivatedRoute, private router: Router, private location: Location, private examesService: ExamesService, private documentoService: DocumentoService) {}
 
   ngOnInit() {
     const tipoAgendamento = this.route.snapshot.paramMap.get('tipo');
@@ -29,10 +36,18 @@ export class AdminAnexarDocumentoComponent {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.selectedFileName = input.files[0].name;
+      this.selectedFile = input.files[0];
+      this.fileErrors = false;
     } else {
       this.selectedFileName = null;
+      this.fileErrors = true;
     }
   }
+
+  saveURL(event: any) {
+    this.urlexterna = event.target.value;
+    console.log(this.urlexterna);
+}
 
   selectType(type: string) {
     this.selectedType = type;
@@ -43,14 +58,62 @@ export class AdminAnexarDocumentoComponent {
   }
 
   anexarNoAgendamento() {
-    Swal.fire({
-        title: "O documento foi anexado ao agendamento",
-        imageUrl: "/assets/images/joiaconcluido.png",
-        imageWidth: 250,
-        imageHeight: 200,
-        imageAlt: "Registro inserido icone",
-        confirmButtonColor: "#0099B9",
-        confirmButtonText: "Concluído",
+    if(this.urlexterna && this.tipoAgendamento == "exame"){
+      this.examesService.adicionarResultadoUrlExterna(Number(this.agendamento), this.urlexterna).subscribe({
+        next: (exame: ExameConcluidoResponse) => {
+          Swal.fire({
+            title: "A URL foi anexada ao exame",
+            imageUrl: "/assets/images/joiaconcluido.png",
+            imageWidth: 250,
+            imageHeight: 200,
+            imageAlt: "Registro inserido icone",
+            confirmButtonColor: "#0099B9",
+            confirmButtonText: "Concluído",
+          });
+        },
+        error: (error) => {
+          alert('falha ao anexar!'),
+          this.fileErrors = true;
+        }
       });
     }
+    if (this.selectedFile && this.tipoAgendamento == "exame") {
+      this.examesService.anexarResultado(Number(this.agendamento), this.selectedFile).subscribe({
+        next: (exame: ExameConcluidoResponse) => {
+          Swal.fire({
+            title: "O documento foi anexado ao exame",
+            imageUrl: "/assets/images/joiaconcluido.png",
+            imageWidth: 250,
+            imageHeight: 200,
+            imageAlt: "Registro inserido icone",
+            confirmButtonColor: "#0099B9",
+            confirmButtonText: "Concluído",
+          });
+        },
+        error: (error) => {
+          alert('falha ao anexar!'),
+          this.fileErrors = true;
+        }
+      });
+    } else if (this.selectedFile && this.tipoAgendamento == "consulta"){
+      this.documentoService.anexarArquivo(Number(this.agendamento), this.selectedFile).subscribe({
+        next: (exame: Documento) => {
+          Swal.fire({
+            title: "O documento foi anexado a consulta",
+            imageUrl: "/assets/images/joiaconcluido.png",
+            imageWidth: 250,
+            imageHeight: 200,
+            imageAlt: "Registro inserido icone",
+            confirmButtonColor: "#0099B9",
+            confirmButtonText: "Concluído",
+          });
+        },
+        error: (error) => {
+          alert('falha ao anexar!'),
+          this.fileErrors = true;
+        }
+      });
+    }
+    this.router.navigate(['/admin']);
+  }
 }
