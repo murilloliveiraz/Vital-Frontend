9a42a846866a686c5f3227f7a4d01f4753da5321
Murillo Oliveira <murillo1047@gmail.com>
2024-11-09 22:38:56 -0300

    refactor: download service

--- src/app/services/Download.service.ts
+++ src/app/services/Download.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +8,24 @@ export class DownloadService {
   constructor(private http: HttpClient) {}
 
   downloadFile(url: string, filename: string) {
-    this.http.get(url, { responseType: 'blob' }).subscribe((blob) => {
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = filename;
-      link.click();
+    this.http.get(url, {
+        responseType: 'blob',
+        headers: new HttpHeaders({ skipInterceptor: '' })
+    }).subscribe({
+        next: (blob) => {
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(link.href);
+            setTimeout(() => {
+              link.remove();
+          }, 100);
+        },
+        error: (err) => console.error('Erro ao baixar o arquivo:', err)
     });
-  }
+}
+
 }
