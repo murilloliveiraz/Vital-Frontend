3cc16380d13e3aaf93e22c91e3926423bbe04534
Murillo Oliveira <murillo1047@gmail.com>
2024-11-09 22:35:24 -0300

    feat: adicionar serviço para gerenciamento de documentos

---
+++ src/app/services/documento.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment';
+import { Documento } from '../models/documento';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentoService {
+  private baseUrl = `${environment.endPoint}/documentos`
+
+  constructor(private http: HttpClient) {}
+
+  anexarArquivo(id: number, file: File): Observable<Documento> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post<Documento>(`${this.baseUrl}/${id}/anexar-arquivo`, formData);
+  }
+}
