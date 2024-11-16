ca724aa408b0a4e3d7c91b5cf67eb8cb46cbffde
Murillo Oliveira <murillo1047@gmail.com>
2024-11-15 21:14:05 -0300

    feat: adding a input to select at which hospital the doctor will work

--- src/app/pages/admin-cadastrar-medico/admin-cadastrar-medico.component.ts
+++ src/app/pages/admin-cadastrar-medico/admin-cadastrar-medico.component.ts
@@ -1,9 +1,11 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HospitalResponseContract } from 'src/app/models/hospital/hospitalResponseContract';
 import { MedicoRequestContract } from 'src/app/models/medico/medicoRequestContract';
 import { MedicoResponseContract } from 'src/app/models/medico/medicoResponseContract';
 import { AuthService } from 'src/app/services/auth.service';
+import { HospitalService } from 'src/app/services/hospital.service';
 import { MedicoService } from 'src/app/services/medico.service';
 import Swal from 'sweetalert2';
 
@@ -14,8 +16,17 @@ import Swal from 'sweetalert2';
 })
 export class AdminCadastrarMedicoComponent {
   email: string = "";
-  hospitalId: number = 1;
-  constructor(public medicoService: MedicoService ,private location: Location, public formBuilder: FormBuilder, private authService: AuthService) {}
+  hospitalId: number;
+  hospitais: HospitalResponseContract[];
+  filteredHospitalList: HospitalResponseContract[] = [];
+  hospitalInput: string = '';
+  showHospitais = false;
+
+  constructor(public medicoService: MedicoService,
+    private location: Location,
+    public formBuilder: FormBuilder,
+    private authService: AuthService,
+    private hospitalService: HospitalService) {}
   cadastroForm: FormGroup;
 
   ngOnInit(): void {
@@ -27,8 +38,18 @@ export class AdminCadastrarMedicoComponent {
         cpf: ['', [Validators.required]],
         crm: ['', [Validators.required]],
         especialidade: ['', [Validators.required]],
+        hospitalInput: ['', [Validators.required]],
       }
     )
+    const observerDeHospitais = {
+      next: (data: HospitalResponseContract[]) => {
+        this.hospitais = data;
+      },
+      error: (err: any) => {
+        alert('Ocorreu um erro');
+      }
+    };
+    this.hospitalService.getAll().subscribe(observerDeHospitais);
   }
 
   get dadosForm(){
@@ -68,5 +89,22 @@ export class AdminCadastrarMedicoComponent {
     }
 
     this.medicoService.create(medico).subscribe(observer);
+  }
+
+  filterHospitalList() {
+    const input = this.hospitalInput.replace(/\D/g, '');
+    this.filteredHospitalList = this.hospitais.filter(hospital =>
+      hospital.nome.replace(/\D/g, '').includes(input)
+    );
+  }
+
+  selectHospital(hospital: HospitalResponseContract) {
+    this.hospitalInput = hospital.nome;
+    this.hospitalId = hospital.hospitalId;
+    this.showHospitais = false;
+  }
+
+  hideHospitais() {
+    setTimeout(() => this.showHospitais = false, 200);
   }
 }
