31a5c36a1a729ecad89cedaa0ac08b3085eb9ac4
Murillo Oliveira <murillo1047@gmail.com>
2024-11-15 21:07:12 -0300

    refactor: removing user name and profile picture

--- src/app/components/menu-adm/menu-adm.component.ts
+++ src/app/components/menu-adm/menu-adm.component.ts
@@ -6,7 +6,7 @@ import { SideNavService } from 'src/app/services/SideNav.service';
   templateUrl: './menu-adm.component.html',
   styleUrls: ['./menu-adm.component.css']
 })
-export class MenuAdmComponent implements OnInit {
+export class MenuAdmComponent {
   @Input() isOpen = false; // Accept isOpen as an input property
 
   profileImage: string = '';
@@ -16,16 +16,6 @@ export class MenuAdmComponent implements OnInit {
     this.sidenavService.sidenavOpen$.subscribe(isOpen => {
       this.isOpen = isOpen;
     });
-  }
-
-  ngOnInit(): void {
-    // Recuperar os dados do sessionStorage
-    const storedImage = sessionStorage.getItem('profileImage');
-    const storedName = sessionStorage.getItem('profileName');
-
-    // Atribuir os valores às propriedades ou valores padrão
-    this.profileImage = storedImage ? storedImage : 'assets/images/components/admin/menu/default-admin-image.jpg'; // Caminho padrão para a imagem
-    this.profileName = storedName ? storedName : 'Luana Ribeiro'; // Nome padrão
   }
 
   toggleSidenav() {
