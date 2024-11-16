a9c51348913dc0dc75a40ac33b4180814f460e58
Murillo Oliveira <murillo1047@gmail.com>
2024-11-15 21:11:38 -0300

    refactor: removing unused input propeties and sending an service icon to improve ux

--- src/app/components/paciente-servicos/paciente-servicos.component.ts
+++ src/app/components/paciente-servicos/paciente-servicos.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import Splide from '@splidejs/splide';
+import { ServicoResponseContract } from 'src/app/models/servico/servicoResponseContract';
+import { ServicoService } from 'src/app/services/servico.service';
 
 @Component({
   selector: 'app-paciente-servicos',
@@ -7,18 +9,44 @@ import Splide from '@splidejs/splide';
   styleUrls: ['./paciente-servicos.component.css']
 })
 export class PacienteServicosComponent {
+  servicos: ServicoResponseContract[] = [];
 
-  constructor() { }
+  constructor(public servicoService: ServicoService) {}
 
   ngOnInit(): void {
-    var splide = new Splide( '#pacientes-servicos-slider', {
-      perPage: 3,
-      focus  : 0,
-      width: "100%",
-      omitEnd: true,
-      arrows: false
-    } );
-
-    splide.mount();
+    this.servicoService.getAll().subscribe((data: ServicoResponseContract[]) => {
+      this.servicos = data;
+    });
   }
+
+
+  servicosHospitalares = [
+    // Exames
+    { nome: 'Hemograma', icone: '/assets/images/servicos/hemograma.svg' },
+    { nome: 'Raio-X', icone: '/assets/images/servicos/RaioX.svg' },
+    { nome: 'Exame de Urina', icone: '/assets/images/servicos/Urina.svg' },
+    { nome: 'Eletrocardiograma', icone: '/assets/images/servicos/Eletocardiogama.svg' },
+    { nome: 'Teste de Covid-19', icone: '/assets/images/servicos/TesteVirus.svg' },
+    { nome: 'Exame de DST', icone: '/assets/images/servicos/DST.svg' },
+    { nome: 'Exame Gastrointestinal', icone: '/assets/images/servicos/Gastrointestinologia.svg' },
+
+    // Consultas
+    { nome: 'Clínica Geral', icone: '/assets/images/servicos/default.svg' },
+    { nome: 'Pediátrica', icone: '/assets/images/servicos/Pediatra.svg' },
+    { nome: 'Cardiologia', icone: '/assets/images/servicos/Cardiologia.svg' },
+    { nome: 'Dermatologia', icone: '/assets/images/servicos/Dermatologia.svg' },
+    { nome: 'Endocrinologia', icone: '/assets/images/servicos/Endocrinologia.svg' },
+    { nome: 'Ginecologia', icone: '/assets/images/servicos/Ginecologia.svg' },
+    { nome: 'Neurologia', icone: '/assets/images/servicos/Neurologia.svg' },
+    { nome: 'Nutricao', icone: '/assets/images/servicos/Nutricao.svg' },
+    { nome: 'Ortopedia', icone: '/assets/images/servicos/Ortopedia.svg' },
+    { nome: 'Oftalmologia', icone: '/assets/images/servicos/Oftalmologia.svg' },
+    { nome: 'Psiquiatria', icone: '/assets/images/servicos/Psicologia.svg' }
+  ];
+
+  obterIconeServico(nomeServico: string): string | undefined {
+    const servico = this.servicosHospitalares.find(s => s.nome === nomeServico);
+    return servico ? servico.icone : '/assets/images/servicos/default-icon.svg';
+  }
+
 }
