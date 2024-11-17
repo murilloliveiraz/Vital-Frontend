a99215670b36685a56a844b2da3a954ddb82b2ec
Murillo Oliveira <murillo1047@gmail.com>
2024-11-17 16:31:05 -0300

    feat: adding 'delete' button to colaborators card

--- src/app/components/colaborador-card/colaborador-card.component.ts
+++ src/app/components/colaborador-card/colaborador-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { AdminResponseContract } from 'src/app/models/administrador/adminResponseContract';
+import { AdministradorService } from 'src/app/services/administrador.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-colaborador-card',
@@ -8,4 +10,20 @@ import { AdminResponseContract } from 'src/app/models/administrador/adminRespons
 })
 export class ColaboradorCardComponent {
   @Input() colaborador: AdminResponseContract;
+
+  constructor(private adminService: AdministradorService){}
+
+  excluirColaborador(colab: AdminResponseContract){
+    this.adminService.deleteAdmin(colab.id).subscribe(() => {
+      Swal.fire({
+        title: "O colaborador foi excluído!",
+        imageUrl: "/assets/images/joiaconcluido.png",
+        imageWidth: 250,
+        imageHeight: 200,
+        imageAlt: "Registro inserido icone",
+        confirmButtonColor: "#0099B9",
+        confirmButtonText: "Concluído",
+      });
+    });
+  }
 }
