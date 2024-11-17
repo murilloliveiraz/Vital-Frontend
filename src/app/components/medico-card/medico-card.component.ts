9c0d020107f6766fa2c5fcc87e3fe776bce983b5
Murillo Oliveira <murillo1047@gmail.com>
2024-11-17 16:31:23 -0300

    feat: adding 'delete' button to doctors card

--- src/app/components/medico-card/medico-card.component.ts
+++ src/app/components/medico-card/medico-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { MedicoResponseContract } from 'src/app/models/medico/medicoResponseContract';
+import Swal from 'sweetalert2';
+import { MedicoService } from './../../services/medico.service';
 
 @Component({
   selector: 'app-medico-card',
@@ -6,11 +9,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./medico-card.component.css']
 })
 export class MedicoCardComponent {
-  @Input() nome: string = '';
-  @Input() especialidade: string = '';
-  @Input() crm: string = '';
-  @Input() cpf: string = '';
-  @Input() email: string = '';
-  @Input() ativo: boolean = true;
+  @Input() medico: MedicoResponseContract;
   @Input() isADM: boolean = false;
+
+  constructor(private medicoService: MedicoService){}
+
+  excluirMedico(medico: MedicoResponseContract){
+    this.medicoService.delete(medico.id).subscribe(() => {
+      Swal.fire({
+        title: "O médico foi excluído!",
+        imageUrl: "/assets/images/joiaconcluido.png",
+        imageWidth: 250,
+        imageHeight: 200,
+        imageAlt: "Registro inserido icone",
+        confirmButtonColor: "#0099B9",
+        confirmButtonText: "Concluído",
+      });
+    });
+  }
 }
