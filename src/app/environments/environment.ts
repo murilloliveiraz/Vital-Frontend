2800dd24537d069fdfc7a6cc5b3236dce6e42551
Murillo Oliveira <murillo1047@gmail.com>
2024-11-21 15:48:31 -0300

    fix: env file

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
-export const environment  = {
+export const environment = {
   production: true,
-  endPoint: '',
-  clientId: "",
-  mercadoPagoAPIKey: ""
-}
+  endPoint: process.env["NEXT_PUBLIC_API_URL"],
+  clientId: process.env["NEXT_PUBLIC_CLIENT_ID"],
+  mercadoPagoAPIKey: process.env["NEXT_PUBLIC_MERCADOPAGO_API_KEY"],
+};
