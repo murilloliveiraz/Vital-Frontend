391767121fc7df2859c19493e13ad465cb345b26
Murillo Oliveira <murillo1047@gmail.com>
2024-11-21 15:03:58 -0300

    feat: scripts to help deploy at vercel

---
+++ src/app/scripts/replace-env.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const targetPath = './src/environments/environment.prod.ts';
+
+const environmentFileContent = `
+  export const environment = {
+    production: true,
+    apiUrl: "${process.env.endPoint}"
+  };
+`;
+
+fs.writeFile(targetPath, environmentFileContent, (err) => {
+  if (err) {
+    console.error('Erro ao escrever o arquivo de ambiente', err);
+  } else {
+    console.log('Arquivo de ambiente atualizado com sucesso!');
+  }
+});
