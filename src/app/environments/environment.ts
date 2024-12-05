dbb2bd6dd9e0055e96e6f9805856c6da411548ed
Murillo Oliveira <murillo1047@gmail.com>
2024-12-05 19:45:41 -0300

    removing env file

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
-export const environment  = {
-  production: false,
-  endPoint: 'http://localhost:5051/api',
-  clientId: '583197098397-p9ne753ev2r9rb9ldvpuq2klgbtkl3vd.apps.googleusercontent.com',
-  mercadoPagoAPIKey:'APP_USR-b5d768b7-dd4f-4eac-aee4-4cc97b4d78aa'
-}
+export const environment = {
+  production: true,
+  endPoint: process.env["ENDPOINT"],
+  clientId: process.env["CLIENT_ID"],
+  mercadoPagoAPIKey: process.env["MERCADOPAGO"],
+};
